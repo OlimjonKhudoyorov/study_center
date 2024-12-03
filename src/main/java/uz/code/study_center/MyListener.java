11d8c141ed094d8bb9ba7977900c5c6fab20e2df
XudoyorovOlimjon <xudoyorovolimjon0302@gmail.com>
2024-12-03 08:35:16 +0500

    first commit

---
+++ src/main/java/uz/code/study_center/MyListener.java
@@ -0,0 +1,17 @@
+package uz.code.study_center;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class MyListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
+    }
+}
---
+++ src/main/java/uz/code/study_center/Todo.java
@@ -0,0 +1,16 @@
+package uz.code.study_center;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private Boolean completed;
+}
